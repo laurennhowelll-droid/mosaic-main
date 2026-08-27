1425494dabea1270a4e6941d678470b6ffc67006
Lauren Christensen <lochristensen@Laurens-MacBook-Pro.local>
2026-08-27 14:38:12 -0600

    Add Google tag to site layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MHZQ736ZDN" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-MHZQ736ZDN');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
         <ClarityCheckPrompt />
